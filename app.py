5b9b9f45a3935d07ad24d4a873f8cac3f61a989c
chayan141 <chayan.paul.141@gmail.com>
2024-09-16 10:47:07 +0530

    Added Feature for Division

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from calc_fun import do_addition, do_subtraction
+from calc_fun import do_addition, do_subtraction, do_division
 from multiply import do_multiplication
 def main():
     print('Welcome to the calculator app')
@@ -6,6 +6,7 @@ def main():
           1. Add
           2. Subtract
           3. Multiply
+          4.Division
           ''')
     
     user_input = str(input('select the input'))
@@ -19,6 +20,13 @@ def main():
 
     elif user_input == "3":
         result = do_multiplication(a,b)
+
+    elif user_input == '4':
+        result = do_division(a,b)
+
+    
+
+    
 
     print('Result: ', result)
 
--- calc_fun.py
+++ calc_fun.py
@@ -5,3 +5,9 @@ def do_addition(a:int,b:int):
 
 def do_subtraction(a:int,b:int):
     return a-b
+
+def do_division(a:int, b:int):
+    try:
+        return a/b
+    except ZeroDivisionError as e:
+        return "Can not divided by Zero"
